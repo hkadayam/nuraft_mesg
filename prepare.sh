2202218b843bb789ebe247f50ceff213dc94f4c6
Hari Kadayam <hkadayam@ebay.com>
2025-05-26 17:03:46 -0700

    Updated the github flows to conan2

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,14 @@
 set -eu
 
+conan_major=`conan --version | awk '{print $3}' | awk -F'.' '{print $1}'`
+echo "Using conan major version: ${conan_major}"
+
 echo -n "Exporting custom recipes..."
 echo -n "nuraft."
-conan export 3rd_party/nuraft nuraft/2.4.5@
+
+if [ $conan_major -eq 1 ] ; then
+    conan export 3rd_party/nuraft nuraft/2.4.5@
+else
+    conan export 3rd_party/nuraft --name nuraft --version 2.4.5 >/dev/null
+fi
 echo "done."
